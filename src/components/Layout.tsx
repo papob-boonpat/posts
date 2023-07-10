4669580d5b0d8d36dccddb4f5860fe3bd4236b87
papob.boonpat <papobboonpat@txm089557a.localdomain>
2023-07-11 00:54:32 +0700

    Post State

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Layout() {
   return (
     <div>
       <nav className="sticky top-0 h-16 w-full bg-white shadow-lg flex items-center px-6 text-main">
-        <h1 className="text-3xl font-bold">Post It</h1>
+        <Link to="/">
+          <h1 className="text-3xl font-bold">Post It</h1>
+        </Link>
       </nav>
       <main className="w-full max-w-3xl p-4 mx-auto">
         <Outlet />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import PostFieldCard from "../components/PostFieldCard";
-import PostCard from "../components/PostCard";
+import PostCard, { PostCardLoading } from "../components/PostCard";
 import axios from "axios";
 
 function Home() {
   const [posts, setPosts] = useState<Post[]>([]);
-  useEffect(() => {
+  const [state, setFetch] = useState<"loading" | "error" | "none">("none");
+  const getData = useCallback(() => {
+    setFetch("loading");
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
       .then((res) => {
         setPosts(res.data);
+        setFetch("none");
+      })
+      .catch(() => {
+        setFetch("error");
       });
   }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
   return (
     <div className="flex flex-col gap-4">
@@ -19,6 +29,26 @@ function Home() {
       {posts.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
+      {state === "loading" ? (
+        <>
+          <PostCardLoading />
+          <PostCardLoading />
+          <PostCardLoading />
+        </>
+      ) : null}
+      {state === "error" ? (
+        <div className="text-red-500">
+          Failed to load please try agian.{" "}
+          <span
+            className="underline text-main cursor-pointer"
+            onClick={() => {
+              getData();
+            }}
+          >
+            try again
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 }
