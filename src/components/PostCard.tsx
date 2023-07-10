94a6224c32720aa3d2a6937e55818b6c478353e0
papob.boonpat <papobboonpat@txm089557a.localdomain>
2023-07-10 23:30:22 +0700

    Post Card

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PostCard() {
+  return (
+    <div className="bg-white p-6 rounded-2xl shadow-md">
+      <h2 className="text-2xl">Title</h2>
+      <p className="mt-3">Description</p>
+    </div>
+  );
+}
+
+export default PostCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PostFieldCard from "../components/PostFieldCard";
+import PostCard from "../components/PostCard";
 
 function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <PostFieldCard />
+      <PostCard />
     </div>
   );
 }
